76b3235caae15fa59ede88d3ef55f8376c20487a
m8man <moritz.achtmann+git@gmail.com>
2025-09-07 12:46:35 +0000

    Init m8pkgs

--- flake.nix
+++ flake.nix
@@ -2,12 +2,23 @@
   description = "nix-library: dev environment flake templates";
 
   outputs = { self }: {
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }@inputs: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system;};
+    m8pkgs = import ./pkgs { inherit pkgs; };
+  in
+  {
     templates = {
       bevy = {
         path = ./templates/bevy;
         description = "Rust / Bevy Engine development environment";
       };
     };
+    packages.${system} = m8pkgs;
   };
 }
 
